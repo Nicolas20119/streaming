7e080d4475b741f9cf448728077363125889284f
Nicolas Gutierrez <nicolasgutierres11@hotmail.com>
2023-10-05 16:40:59 -0500

    Cambios 5 de octubre

--- resources/views/users/create.blade.php
+++ resources/views/users/create.blade.php
@@ -6,7 +6,7 @@
             <div class="row mb-2">
                 <div class="col-sm-12">
                     <h1>
-                        Edit Qrcode
+                    Create Users
                     </h1>
                 </div>
             </div>
@@ -19,17 +19,19 @@
 
         <div class="card">
 
-            {!! Form::model($qrcode, ['route' => ['qrcodes.update', $qrcode->id], 'method' => 'patch','enctype' => 'multipart/form-data']) !!}
+            {!! Form::open(['route' => 'users.store']) !!}
 
             <div class="card-body">
+
                 <div class="row">
-                    @include('qrcodes.fields')
+                    @include('users.fields')
                 </div>
+
             </div>
 
             <div class="card-footer">
                 {!! Form::submit('Save', ['class' => 'btn btn-primary']) !!}
-                <a href="{{ route('qrcodes.index') }}" class="btn btn-default"> Cancel </a>
+                <a href="{{ route('users.index') }}" class="btn btn-default"> Cancel </a>
             </div>
 
             {!! Form::close() !!}
